cd139c63abee5105de33f37599a45bcf8643e9cb
Evgeny Turmy <jenqa29@mail.ru>
2021-11-07 09:13:00 +0200

    Project added

---
+++ src/App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        String name;
+        int age;
+        double salary;
+
+        System.out.println("Hello. What is your name?");
+
+        name = input.next();
+
+        System.out.println();
+        System.out.println("Hi, " + name + "! How old are you?");
+
+        age = input.nextInt();
+
+        System.out.println();
+        System.out.println("So you're " + age + ", eh? That's not old at all!");
+        System.out.println("How much do you make, " + name + "?");
+
+        salary = input.nextDouble();
+        
+        System.out.println();
+        System.out.println(salary + "! I hope that's per hour and not per year! LOL!");
+    }
+}
